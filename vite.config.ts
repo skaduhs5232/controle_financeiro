2f9ef61076940316b050c6dbb96584175ba2db8a
thiago <thiagooliveira1039@gmail.com>
2025-01-28 00:18:24 -0300

    Fix module loading issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/controle_financeiro/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  plugins: [react(), ],
 })
