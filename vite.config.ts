1dcf5f677de98a39476018b23827f3fcf1d31cbb
Thiago Sampaio <thiagooliveira1039@gmail.com>
2025-01-28 09:56:44 -0300

    Ajustes para deploy no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
@@ -23,4 +23,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   }
-})
+}) */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/controle_financeiro/', // Substitua pelo nome do seu repositório
+});
