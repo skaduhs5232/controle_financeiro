fc05aca00d3a959783c0e3b3f2d9d54d6d357bb2
thiago <thiagooliveira1039@gmail.com>
2025-01-27 23:30:22 -0300

    Update build configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/controle_financeiro/', // nome do seu repositório
+  base: '/controle_financeiro/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
